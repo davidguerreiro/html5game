d2f1f9818a359ddee6083ce370e2cf55beb2d49e
davidguerreiro <guerrebasket@hotmail.es>
2017-05-18 16:36:03 +0100

    add play state

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 
+var PlayState : {};
+
 // init phaser
 window.onload = function () {
     let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+    game.state.add( 'play', PlayState );
+    game.state.start( 'play' );
 };
