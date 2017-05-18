5863d4f1dcaefb3434a2c52a68688ccfc256ef61
davidguerreiro <guerrebasket@hotmail.es>
2017-05-18 17:12:41 +0100

    add hero sprite in the init level

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
+/**
+ * Create hero
+ */
+function Hero( game, x, y ) {
+
+	// class Phaser.Sprite constructor
+	Phaser.Sprite.call( this, game, x, y, 'hero' );
+
+}
+
+// inherit from Phaser.Sprite
+Hero.prototype             = Object.create( Phaser.Sprite.prototype );
+Hero.prototype.constructor =  Hero;
 
 PlayState = {};
 
 // load game assets here
 PlayState.preload = function() {
+
+	// load hero image
+	this.game.load.image( 'hero', 'images/hero_stopped.png' );
 
 	// preload background image
 	this.game.load.image( 'background', 'images/background.png' );
@@ -10,7 +26,7 @@ PlayState.preload = function() {
   // load level 1 data
   this.game.load.json( 'level:1', 'data/level01.json' );
   this.game.load.image( 'ground', 'images/ground.png' );
-	this.game.load.image( 'ground', 'images/grass_8x1.png' );
+	this.game.load.image('grass:8x1', 'images/grass_8x1.png' );
 	this.game.load.image('grass:6x1', 'images/grass_6x1.png');
   this.game.load.image('grass:4x1', 'images/grass_4x1.png');
   this.game.load.image('grass:2x1', 'images/grass_2x1.png');
@@ -27,16 +43,31 @@ PlayState.create = function() {
 	this._loadLevel( this.game.cache.getJSON( 'level:1' ) );
 }
 
+/**
+ * Check the methods defined below load level as load level
+ * loginc has been splited
+ */
 PlayState._loadLevel = function( data ) {
 
 	// spawn all platforms
 	data.platforms.forEach( this._spawnPlatform, this );
+
+	// spawn hero and enemies
+	this._spawnCharacters( { hero: data.hero } );
 };
 
 // spawn platforms from data coming from the JSON file
 PlayState._spawnPlatform = function( platform ) {
 	this.game.add.sprite( platform.x, platform.y, platform.image );
 }
+
+// spawn hero and enemies data coming from the JSON file
+PlayState._spawnCharacters = function( data ) {
+
+	// spawn hero
+	this.hero = new Hero( this.game, data.hero.x, data.hero.y );
+	this.game.add.existing( this.hero );
+};
 
 // init phaser
 window.onload = function () {
